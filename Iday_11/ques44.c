a4101b671cd847dbc263efaad8e6a4868e029770
neelimagupta-byte <neelimagupta4115@gmail.com>
2026-06-07 10:24:56 +0530

    Add files via upload

---
+++ Iday_11/ques44.c
@@ -0,0 +1,16 @@
+// function to find factorial
+#include<stdio.h>
+int factorial(int n){
+    int m,fac=1;
+    m=n;
+    while(n>0){
+        fac*=n;
+        n--;}
+    return fac;
+}
+int main(){
+    int n;
+    printf("enter a number : ");
+    scanf("%d",&n);
+    printf("factorial of %d is %d.",n,factorial(n));
+}
